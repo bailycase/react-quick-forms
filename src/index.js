8ee363ac4a2f783b72b2543209b46ac4e0a3af90
Baily Case <bcase@mertz.local>
2019-09-26 12:27:29 -0500

    Package name change

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+const Form = () => {
+  return (
+    <form>
+      <h1>Form Header</h1>
+    </form>
+  )
 }
+
+export default Form
